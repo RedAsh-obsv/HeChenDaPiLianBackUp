79bdfb128fda5836a6ee1063abaae4b7d97ef92f
XuDongsheng-obsv <XuDongsheng_obsv@outlook.com>
2021-08-09 10:46:44 +0800

    源码开源分享.使用版本为2018.4.2及以上.打包到安卓请提前安装好SDK环境

---
+++ HeChenDaPiLian/Assets/Scripts/Item.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+[System.Serializable]
+public class Item
+{
+    public int Lv;
+    public int Mass;
+    public Color color;
+    public Sprite sprite;
+    public float Scale;
+}
